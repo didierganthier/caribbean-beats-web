0ff1519a44ad477c64e914a25953067100d25757
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-10-03 17:07:25 -0400

    Design Login Page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
-  return (
-    <div className='mt-4'>
-        <h1 className='text-4xl text-center'>Login</h1>
-        <form className='max-w-md mx-auto'>
-            <input type="email" name="" id="" placeholder='your@email.com'/>
-            <input type="password" name="" id="" placeholder='password'/>
-            <button className='primary'>Login</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='mt-4 grow flex items-center justify-around '>
+            <div className='mb-64'>
+                <h1 className='text-4xl text-center mb-4'>Login</h1>
+                <form className='max-w-md mx-auto'>
+                    <input type="email" name="" id="" placeholder='your@email.com' />
+                    <input type="password" name="" id="" placeholder='password' />
+                    <button className='primary'>Login</button>
+                    <div className='text-center py-2 text-gray-500'>
+                        Don't have an account?  
+                        <Link to='/register' className='underline text-black'> Register now</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
